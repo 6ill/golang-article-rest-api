3c0cdc005a7d34469f44cc1acdd3ad52198a98b3
6ill <kyrbill123@gmail.com>
2025-06-12 09:26:17 +0700

    add/graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/6ill/go-article-rest-api/internal/helper"
 	"github.com/6ill/go-article-rest-api/internal/infrastructure"
@@ -18,7 +21,23 @@ func main() {
 
 	api.HttpRouteInit(app, container)
 	port := fmt.Sprintf("%s:%d", container.App.ServerHost, container.App.ServerPort)
-	if err := app.Listen(port); err != nil {
-		helper.Logger(helper.LoggerLevelFatal, "error", err)
-	}
+
+	go func() {
+		if err := app.Listen(port); err != nil {
+			helper.Logger(helper.LoggerLevelFatal, "error", err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c
+	fmt.Println("Gracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+
+	// Close database
+	container.Db.Close()
+	fmt.Println("App was successful shutdown.")
 }
